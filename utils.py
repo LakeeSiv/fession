aaf046e4222f1b32382fced26c70e9e266c5e549
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-02-15 23:41:51 +0000

    added utils

---
+++ utils.py
@@ -0,0 +1,26 @@
+import csv
+
+
+def post_number(hashtags, index):
+    try:
+        num = int(hashtags[index][11:])
+        return num
+    except ValueError:
+        raise Exception("The string could not be converted into an integer")
+
+
+def get_credentials():
+
+"""
+currently login does not work
+
+"""
+
+with open("logindetails.csv", "r") as file:
+    details = csv.reader(file)
+    for detail in details:
+
+        email = detail[0]
+        password = detail[1]
+
+    return (email, password)
