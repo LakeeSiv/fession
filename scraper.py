d538d453133e2f6c45f37fb31c105a2cfd45a9a9
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-02-16 18:01:12 +0000

    added storing to database

--- scraper.py
+++ scraper.py
@@ -1,16 +1,33 @@
 from facebook_scraper import get_posts
 from PostClass import PostClass
+import sqlite3
 
 GROUP = "realoxfess"
+# GROUP = "leedsfess"
 
+conn = sqlite3.connect(f"{GROUP}.db")
+c = conn.cursor()
+# c.execute("""CREATE TABLE posts(postnum INT, text TEXT, sentiment FLOAT)""")
 
-all_posts = get_posts(GROUP, pages=20)
+all_posts = get_posts(GROUP, pages=1000)
 next(all_posts)  # ignore the page description
 
 
 for post in (all_posts):
     Post = PostClass(post, GROUP)
-    print(post["text"],"\n","--------------------------------------------------------------------")
+    try:
+        postnum = Post.get_post_number()
+        text = Post.get_post_text()
+        sentiment = Post.get_setiment()
 
-    print(Post.get_post_number(),"\n","--------------------------------------------------------------------")
-    print(Post.get_setiment())
+        c.execute("""INSERT INTO posts VALUES(?,?,?)""",(postnum,text,sentiment))
+    except:
+        pass
+
+    # print(Post.get_setiment(),Post.get_post_number(),"\n")
+    # print(Post.get_post_text(),"\n","--------------------------------------------------------------------")
+
+conn.commit()
+
+c.execute("""SELECT postnum FROM posts""")
+print(c.fetchall())
