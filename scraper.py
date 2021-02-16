939c27cdf5981f81edcb862d3abc806ddb5fa7d5
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-02-16 23:45:54 +0000

    updates database with new posts

--- scraper.py
+++ scraper.py
@@ -9,7 +9,14 @@ conn = sqlite3.connect(f"{GROUP}.db")
 c = conn.cursor()
 # c.execute("""CREATE TABLE posts(postnum INT, text TEXT, sentiment FLOAT)""")
 
-all_posts = get_posts(GROUP, pages=1000)
+
+c.execute("""SELECT postnum FROM posts""")
+post_num_list = c.fetchall()
+post_num_list = [tuple[0] for tuple in post_num_list]
+max_post_num = max(post_num_list)
+
+
+all_posts = get_posts(GROUP, pages=100)
 next(all_posts)  # ignore the page description
 
 
@@ -17,17 +24,21 @@ for post in (all_posts):
     Post = PostClass(post, GROUP)
     try:
         postnum = Post.get_post_number()
+        print(postnum, "\n")
+
+        if postnum <= max_post_num:
+            print("Added all the new posts to the database")
+            break
+
         text = Post.get_post_text()
         sentiment = Post.get_setiment()
 
-        c.execute("""INSERT INTO posts VALUES(?,?,?)""",(postnum,text,sentiment))
-    except:
+        c.execute("""INSERT INTO posts VALUES(?,?,?)""",
+                  (postnum, text, sentiment))
+    except BaseException:
         pass
 
     # print(Post.get_setiment(),Post.get_post_number(),"\n")
     # print(Post.get_post_text(),"\n","--------------------------------------------------------------------")
 
 conn.commit()
-
-c.execute("""SELECT postnum FROM posts""")
-print(c.fetchall())
