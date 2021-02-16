93f2066caf33f3ada0e89c0b0020c29d2a27dc56
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-02-16 00:20:08 +0000

    got post number extracting working

--- scraper.py
+++ scraper.py
@@ -1,39 +1,20 @@
-# import csv
 from facebook_scraper import get_posts
-
-"""
-currently login does not work
-
-with open("logindetails.csv","r") as file:
-    details = csv.reader(file)
-    for detail in details:
-
-        email = detail[0]
-        password = detail[1]
-
-print(email,password)
-"""
+from utils import post_number
+import re
 
 GROUP = "realoxfess"
 
 
-all_posts = get_posts(GROUP, pages=2)
+all_posts = get_posts(GROUP, pages=20)
 next(all_posts)  # ignore the page description
-
-def post_number(hashtags,index):
-    try:
-        num = int(hashtags[index][11:])
-        return num
-    except ValueError:
-        raise Exception("The string could not be converted into an integer")
 
 
 for post in (all_posts):
     text = post["text"]
     # print(text,"\n","--------------------------------------------------------------------")
 
-    hashtags = text.split("\n", 2)
+    words = re.split("\n| ", text)
 
-    # if GROUP in hashtags[1]:
-    post_num = post_number(hashtags,0)
-    print(post_num)
+    print(post_number(words, GROUP))
+
+    # print(words,"\n","--------------------------------------------------------------------")
