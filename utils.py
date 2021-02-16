aa11b7e0def405037f320bb488df1de635f986fa
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-02-16 00:16:01 +0000

    updated post number function

--- utils.py
+++ utils.py
@@ -1,16 +1,30 @@
 import csv
 
 
-def post_number(hashtags, index):
-    try:
-        num = int(hashtags[index][11:])
-        return num
-    except ValueError:
-        raise Exception("The string could not be converted into an integer")
+def post_number(words, GROUP):
+    """
+    Args:
+    words: list of words
+    GROUP: group name
+
+    Returns:
+    post_num: post number
+
+    finds the postnumber by searching for the largest number
+    in the GROUP hashtag, which normally looks like
+    f"#{GROUP}{post_num}"
+    """
+    post_num = 0
+    for word in words:
+        word = word.lower()
+        if GROUP in word:
+            number = int(word[1 + len(GROUP):])
+            post_num = max(post_num, number)
+
+    return post_num
 
 
 def get_credentials():
-
     """
     currently login does not work
     """
